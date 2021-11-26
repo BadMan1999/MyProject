5ce0f08e0e0e11c016adaca6cedd51210e80099d
BadMan1999 <ahmed_moon99@hotmail.com>
2021-11-26 17:28:14 +0200

    testt2

---
+++ app/src/main/java/com/example/myproject/AddRecord_activity.java
@@ -0,0 +1,17 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRecord_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_record);
+
+        tools:context=".AddRecord_activity">
+
+    }
+}
---
+++ app/src/main/java/com/example/myproject/Complete_Information_Activity.java
@@ -0,0 +1,14 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Complete_Information_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_information);
+    }
+}
