cf914e11e758f1823a7f990ad9399ec10b5ada08
BadMan1999 <ahmed_moon99@hotmail.com>
2021-12-03 19:02:41 +0200

    testt2

--- app/src/main/java/com/example/myproject/Home.java
+++ app/src/main/java/com/example/myproject/Home.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
 
+    Button Btn_Add_Record,Btn_Add_Food;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Btn_Add_Record = findViewById(R.id.Btn_Add_Record);
+        Btn_Add_Food = findViewById(R.id.Btn_Add_Food);
 
         RecyclerView list;
         Adapter_Home adapter;
@@ -27,6 +35,25 @@ public class Home extends AppCompatActivity {
         list.setLayoutManager(new LinearLayoutManager(this));
         adapter = new Adapter_Home(models, this);
         list.setAdapter(adapter);
+
+
+        Btn_Add_Record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, AddRecord_activity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        Btn_Add_Food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Add_Food_Details_Activity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
