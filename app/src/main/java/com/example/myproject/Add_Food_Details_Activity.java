48f632616fcdf4387255b8d7a507ec6446c7f070
BadMan1999 <ahmed_moon99@hotmail.com>
2021-12-03 19:08:01 +0200

    testt2

--- app/src/main/java/com/example/myproject/Add_Food_Details_Activity.java
+++ app/src/main/java/com/example/myproject/Add_Food_Details_Activity.java
@@ -3,16 +3,36 @@ package com.example.myproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Add_Food_Details_Activity extends AppCompatActivity {
+public class Add_Food_Details_Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
+    String[] co = { " Fruit "," vegetables", "Starchy food", "Dairy"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food_details);
 
+        Spinner spin =  findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(Add_Food_Details_Activity.this);
+
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item, co);
 
 
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        spin.setAdapter(aa);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(), co[position] , Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
